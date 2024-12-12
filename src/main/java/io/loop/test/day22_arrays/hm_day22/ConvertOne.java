89f7a1f3b0c3900d81a8a0327e1e6ab56d81734f
Roam <roman.gribov94@gmail.com>
2024-12-12 03:25:40 +0300

    custom method

--- src/main/java/io/loop/test/day22_arrays/hm_day22/ConvertOne.java
+++ src/main/java/io/loop/test/day22_arrays/hm_day22/ConvertOne.java
@@ -20,23 +20,40 @@ public class ConvertOne {
         System.out.println("Tell me about your name:");
         String num = key.nextLine();
 
-      //  char[] charArray = num.toCharArray();
+        //  char[] charArray = num.toCharArray();
+//        char[] charArray = num.toCharArray();
 
-       char[] charArray = num.toCharArray();
+//               System.out.println(num);
 
- //       System.out.println(num);
+//        System.out.println("Convert: " + Arrays.toString(charArray));
 
-        System.out.println("Convert: " + Arrays.toString(charArray));
+//        for (char num2 : charArray) {
+//            System.out.print(num2 + " ");
+
+//            char [] num3 = num.toCharArray();
+   //         char [] num4 = new char num.length;
+   //         System.out.println( Arrays.equals(1, 5);
+            char firstLetter = num.charAt(0);
+            char lastLetter = num.charAt(num.length() -1);
+
+            System.out.print(firstLetter);
+            System.out.print(lastLetter);
+
+
+
+//            for (int i = 10; i < num.length(); i++) {
+//                if (10 <= charArray.length) {
+//                    System.out.println(num);
+//                } else {
+//                    System.out.println("error");
+//                }
+//            }
 
 //        char [] charArray = new char [num.length()];
 
- //       char [] num1 = {'a', 'b', 'c'};
-  //      String
+            //       char [] num1 = {'a', 'b', 'c'};
+            //      String
 
 
-
-
-
-
+        }
     }
-}
